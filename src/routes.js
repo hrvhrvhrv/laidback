f84e4ebc548731bca7c0df323ef6148396e4826a
Craig Harvey <6125094+hrvhrvhrv@users.noreply.github.com>
2018-04-26 12:54:26 +0100

    This is  git commit

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 import Home from './components/Home.vue';
-import BlogPost from'./components/blog/blogPost.vue';
-
-
+import BlogPost from './components/blog/blogPost.vue';
 
 
 export const routes = [
-    { path: '/', component: Home  },
-    { path: '/blog-post', component: BlogPost  },
-    { path: '/getting-started', component: BlogPost  },
+  {path: '/', component: Home},
+
+  // Main site routes
+  {path: '/getting-started', component: BlogPost},
+  {path: '/about-us', component: BlogPost},
+  {path: '/contact', component: BlogPost},
+
+
+  // Blog routes
+  {path: '/blog-post', component: BlogPost},
+  {path: '/getting-started', component: BlogPost},
+
+  //  Lesson routes
+
 
 ];
